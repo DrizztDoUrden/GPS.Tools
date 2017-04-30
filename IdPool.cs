d22a6a1d6e31515c0d1fd8c37125080adc838626
Vasja Filippov <vasja.filippov94@outlook.com>
2017-04-30 15:15:00 +0300

    Fixed some newlines. Made ids case insensitive.

--- IdPool.cs
+++ IdPool.cs
@@ -2,24 +2,26 @@ using System.Collections.Generic;
 
 namespace GPS.Tools
 {
-    public class IdPool : IIdPool
-    {
-        public Id this[string name] => new Id(this, name);
-        public string this[Id id] => _idToName[id.Value];
-        
-        public int GetRawId(string name)
-        {
-            if (_nameToId.TryGetValue(name, out var id))
-                return id;
-            
-            id = _next++;
-            _nameToId.Add(name, id);
-            _idToName.Add(id, name);
-            return id;
-        }
-        
-        private readonly Dictionary<int, string> _idToName = new Dictionary<int, string>();
-        private readonly Dictionary<string, int> _nameToId = new Dictionary<string, int>();
-        private int _next = 0;
-    }
+	public class IdPool : IIdPool
+	{
+		public Id this[string name] => new Id(this, name);
+		public string this[Id id] => _idToName[id.Value];
+
+		public int GetRawId(string name)
+		{
+			var ntl = name.ToLower();
+
+			if (_nameToId.TryGetValue(ntl, out var id))
+				return id;
+
+			id = _next++;
+			_nameToId.Add(ntl, id);
+			_idToName.Add(id, name);
+			return id;
+		}
+
+		private readonly Dictionary<int, string> _idToName = new Dictionary<int, string>();
+		private readonly Dictionary<string, int> _nameToId = new Dictionary<string, int>();
+		private int _next = 0;
+	}
 }
